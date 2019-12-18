079b689776a3739f3a5b7c8ad1078108a61ec086
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-12-18 16:17:44 -0500

    added server and authored index files

---
+++ index.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+
+const server = require('./server');
+
+const PORT = process.env.PORT || 5001;
+
+server.listen(PORT, () => {
+  console.log(`\n***Server listening on port ${PORT}***\n`);
+});
