82398ea31888a939a467b0b467c854b612ba9d97
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-12-18 16:14:19 -0500

    ran migration and seeded table with data

--- seeds/001users.js
+++ seeds/001users.js
@@ -1,9 +1,9 @@
 exports.seed = function(knex) {
-  return knex('table_name').then(function() {
-    return knex('table_name').insert([
-      { id: 1, username: 'bernard', password: 'pass' },
-      { id: 2, username: 'jackson', password: 'pass' },
-      { id: 3, username: 'jen', password: 'pass' }
+  return knex('users').then(function() {
+    return knex('users').insert([
+      { id: 1, username: 'bernard', password: 'pass', department: 'team lead' },
+      { id: 2, username: 'jackson', password: 'pass', department: 'student' },
+      { id: 3, username: 'jen', password: 'pass', department: 'team lead' }
     ]);
   });
 };
