29fd52cd03612fcd83de262a8189be1378a4bc1e
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-12-18 16:21:29 -0500

    server up

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const server = require('./server');
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 3031;
 
 server.listen(PORT, () => {
   console.log(`\n***Server listening on port ${PORT}***\n`);
