a99b34ed72fb44b03b2c1b9e91ecbfa402455de3
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-12-18 19:12:36 -0500

    register and login routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Login from './components/Login';
 import Register from './components/Register';
+import { Route, Link } from 'react-router-dom';
 
 function App() {
+  const [registered, setRegistered] = useState(true);
+
   return (
     <div className='App'>
-      <Login />
-      <Register />
+      <br />
+      <br />
+      <Link to='login'>Login</Link>
+      <br />
+      <br />
+      <Link to='/register'>Register</Link>
+      <Route path='/login' render={props => <Login {...props} />} />
+      <Route path='/register' render={props => <Register {...props} />} />
     </div>
   );
 }
